f1a778ca9c8baed81e0236001396fa2de99b9755
SANG BEOM KIM <top777777@gmail.com>
2022-10-28 22:50:12 +0900

    2022-10-28

--- colorFlipper/index.js
+++ colorFlipper/index.js
@@ -1,10 +1,13 @@
 const clickMe = document.querySelector("button");
+const color = document.querySelector(".color");
+const screen = document.querySelector(".screen");
+const colors = ["red", "blue", "green"];
+const result = colors[Math.floor(Math.random() * colors.length)];
+console.log(result);
 
 function changeColor() {
-  const screen = document.querySelector(".screen");
-  const color = document.querySelector(".color");
-  screen.style.backgroundColor = "red";
-  color.innerText = `Background Color : Red`;
+  screen.style.backgroundColor = result;
+  color.innerText = `Background Color : ${result}`;
 }
 
 clickMe.addEventListener("click", changeColor);
